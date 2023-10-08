f6778f8d5bcd92b53acf33551feba9eeef9ac478
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-10-08 18:14:30 +0200

    gelateria api fatta

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Menu } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="section-center">
+        <h3>Gelateria</h3>
+        <Menu />
+      </section>
+    </div>
+  );
+}
+
+export default App;
